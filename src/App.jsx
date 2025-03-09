427809469af5a02f39e09807e0d8f72d1b9ac421
hazedrops <hazedrops@hotmail.com>
2025-03-08 21:44:05 -0800

    spinner, proxy, json-server added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+// npm run server
+// npm run dev
+
 import React from 'react'
 import {
   Route,
@@ -7,14 +10,16 @@ import {
 } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout'
 import HomePage from './pages/HomePage'
-import JobsPage from "./pages/JobsPage"
-import NotFoundPage from "./pages/NotFoundPage"
+import JobsPage from './pages/JobsPage'
+import NotFoundPage from './pages/NotFoundPage'
+import JobPage from './pages/JobPage'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
+      <Route path='/jobs/:id' element={<JobPage />} />
       <Route path='*' element={<NotFoundPage />} />
     </Route>
   )
